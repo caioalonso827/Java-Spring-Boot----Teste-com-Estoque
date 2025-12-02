664538a187ada29ebe4e53bf576b4451d2b2a497
Caio Borsato Alonso <caioca827@gmail.com>
2025-12-02 12:20:11 -0300

    Delete src directory

--- src/main/java/com/caiocasa/br/Estoque/Dtos/TransacaoDto.java
+++
@@ -1,9 +0,0 @@
-package com.caiocasa.br.Estoque.Dtos;
-
-import java.time.LocalDate;
-
-import com.caiocasa.br.Estoque.Enums.EnumTipo;
-
-public record TransacaoDto(Integer idProduto, EnumTipo tipoTransacao, Integer quantidade, LocalDate dataTransacao) {
-    
-}
--- src/main/java/com/caiocasa/br/Estoque/Enums/EnumTipo.java
+++
@@ -1,5 +0,0 @@
-package com.caiocasa.br.Estoque.Enums;
-
-public enum  EnumTipo {
-    ENTRADA,SAIDA
-}
