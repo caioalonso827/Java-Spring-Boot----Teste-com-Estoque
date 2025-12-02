ffcf67c6e1818369bcddd00897ec92828349515c
Caio Borsato Alonso <caioca827@gmail.com>
2025-12-02 16:29:30 -0300

    Update Transacao.java

--- src/main/java/com/caiocasa/br/Estoque/Models/Transacao.java
+++ src/main/java/com/caiocasa/br/Estoque/Models/Transacao.java
@@ -12,7 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,7 +23,7 @@ public class Transacao {
     @Column(name="id_transacao")
     private Integer idTrasacao;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name="id_produto")
     private Produto produto;
 
